00119f297998f5ffd94400b5f6b4419cce22e264
Lily.Veinberg <lily.veinberg@gmail.com>
2023-01-06 11:34:36 +0200

    added Leaderb and GameTi

---
+++ Leaderboard.py
@@ -0,0 +1,8 @@
+class Leaderboard:
+
+    def __init__(self, date, dame, word, misses, time):
+        self.word = word
+        self.misses = misses
+        self.dame = dame
+        self.date = date
+        self.time = time
