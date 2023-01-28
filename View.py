ffc73fa74a89f62a80044ec27846c80350a76929
Lily Veinberg <lily.veinberg@gmail.com>
2023-01-28 16:22:37 +0200

    added Leaderb and GameTi mäng peaaegu töötab :) tunni lõpp mäng töötab kodu töö valmis

--- View.py
+++ View.py
@@ -123,7 +123,8 @@ class View(Tk):
         self.center(top)  # center on scree top window
         return frame
 
-    def generate_leaderboard(self, frame, data):
+    @staticmethod
+    def generate_leaderboard(frame, data):
         # Tabel view
         my_table = ttk.Treeview(frame)
 
